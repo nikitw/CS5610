d663bf9007eed4c287e014eaa57e28d54260a240
Nikit Waghela <waghelanikit@gmail.com>
2015-02-16 06:19:26 -0500

    issue #1 #2 #3 addressed
    added 4th and 5th exps and added reference to the Angular basic exp.

---
+++ experiments/js/js-ajax-exp16.js
@@ -0,0 +1,21 @@
+﻿var TEMPLATE;
+$(window).load( function (e) {
+	TEMPLATE = $(".template");
+	getWeather("Boston, US", "metric");
+	getWeather("London, UK", "metric");
+
+});
+
+$(".add").click( function (e) {
+	getWeather($("#cityname").val(), "metric");
+});
+
+function getWeather (city, units) {
+	$.get("http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=60cecb8af5d1dbfa53a99e9ad494520a&units="+units, function (json) {
+		var newCity = TEMPLATE.clone();
+		newCity.find(".city").html(json.name + ", "+ json.sys.country);
+		newCity.find(".temp").html(Math.round(json.main.temp)+"˚C");
+		newCity.find(".wind").html(json.wind.speed+" kmph");
+		newCity.appendTo(".cities");
+	});
+}
---
+++ experiments/js/js-storage-drag-resize.js
@@ -0,0 +1,63 @@
+﻿$(function (e) {
+	$(".dragme").draggable();
+	$(".resizeme").resizable();
+	$(".sortable").sortable({
+		axis: "y"
+	});
+
+	$("#addP").click( function (e) {
+		addPara("new Paragraph", 300, 220);
+	});
+
+	$("#saveP").click( function(e) {
+		save();	
+	});
+
+	$("#loadP").click( function(e) {
+		load();	
+	});
+		
+});
+
+function save() {
+	var para = $(".para");
+	var pList = [];
+	para.each(function (i) {
+		var pos = $(this).position();
+		var text = $(this).text();
+
+		var obj = {
+			text: text,
+			x: pos.left,
+			y: pos.top
+		};
+
+		pList.push(obj);
+	});
+	localStorage.setItem("000-exp17", JSON.stringify(pList));
+}
+
+function load() {
+	var pList = JSON.parse(localStorage.getItem("000-exp17"));
+	if(pList == null) return;
+
+	for (var p in pList) {
+		var para = pList[p];
+		addPara(para.text, para.x, para.y);
+	}
+}
+
+function addPara( text, x, y) {
+	var p = $("<p contenteditable=true>");
+	p.addClass("para");
+	p.draggable({
+		grid: [20,20]
+	});
+	p.css({
+		position: 'absolute',
+		left: x,
+		top:  y
+	});
+	p.html(text);
+	$("#2").append(p);
+}
