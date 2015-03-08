331b23b5f327f31e47961187ded2724522416755
Nikit Waghela <waghelanikit@gmail.com>
2015-03-08 05:09:36 -0400

    added server changes for login and session

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var md5 = require('MD5');
 var app = express();
 
 app.use(express.static(__dirname + "/public/"))
@@ -9,11 +10,23 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.use(multer());
 
 app.use(function(req, res, next) {
+
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With');
+
     next();
 });
+
+
+app.ip = process.env.OPENSHIFT_NODEJS_IP;
+if(app.ip === undefined) {
+    app.ip = '127.0.0.1';
+    app.port = '8888';
+} else
+    app.port = '8080';
+
+var URL = "http://"+app.ip+":"+app.port;
 
 app.get('/', function (req, res) {
   res.render("index.html");
@@ -55,12 +68,222 @@ app.put('/app/artists/:id', function (req, res) {
     res.jsonp(artists);
 });
 
-app.ip = process.env.OPENSHIFT_NODEJS_IP;
-if(app.ip === undefined) {
-    app.ip = '127.0.0.1';
-    app.port = '8888';
-} else
-    app.port = '8080';
+var users = [
+  {name: "admin", username:"admin@neu.edu", password:"admin", hint:"admin", admin:true},
+  {name: "alice", username:"alice@neu.edu", password:"alice", hint:"alice"},
+  {name: "bob", username:"bob@neu.edu", password:"bob", hint:"bob"}
+];
+
+var User = function (name, username, password, hint, admin) {
+  this.name = name;
+  this.username = username;
+  this.password = password;
+  this.hint = hint;
+  this.admin = admin;
+};
+
+
+
+var CHPASS = [];
+var ChPass = function (username) {
+    this.hash = md5(username);
+    this.username = username;
+    this.email = null;
+    this.confirm = false;
+};
+
+var Email = function (to, cc, bcc, from, subject, msgbody, url) {
+  this.to = to;
+  this.cc = cc;
+  this.bcc = bcc;
+  this.from = from;
+  this.subject = subject;
+  this.msgbody = msgbody;
+  this.url = url;
+};
+
+app.get('/app/login', function (req, res) {
+    var user = null;
+    for (var u in users)
+      if(users[u].username == req.query.username &&
+        users[u].password == req.query.password) {
+        user = users[u];
+        break;
+      }
+    if(!user) {
+      user = new User(null,null,null,null,null);
+      user.err = 701;
+      user.errmsg = "Login failed!";
+    }
+
+    res.jsonp(user);
+});
+
+app.post('/app/register', function (req, res) {
+    var username = req.body.username;
+    var auser = null;
+    var user = new User(
+      req.body.name,
+      username,
+      req.body.password,
+      req.body.hint,
+      req.body.admin
+    );
+    for (var u in users)
+      if(users[u].username == username) {
+        auser = users[u];
+        break;
+      }
+    if(auser) {
+      user.err = 702;
+      user.errmsg = "user already exists";
+    } else {
+      users.push(user);
+    }
+    res.jsonp(user);
+});
+
+app.put('/app/update/:id', function (req, res) {
+    var user = new User(
+      req.body.name,
+      req.body.username,
+      req.body.password,
+      req.body.hint,
+      req.body.admin
+    );;
+    var username = req.params.id;
+    var auser = null;
+    var u = 0;
+    for (u in users)
+      if(users[u].username == username) {
+        auser = users[u];
+        break;
+      }
+    if(!auser) {
+      user.err = 703;
+      user.errmsg = "no such user exists";
+    } else {
+      for(var key in user)
+        if(user[key] == null)
+          user[key] = auser[key];
+      for(var c in CHPASS)
+        if(CHPASS[c].username == username) {
+          CHPASS.splice(c,1);
+          break;
+        }
+      users[u] = user;
+    }
+    res.jsonp(user);
+});
+
+app.post('/app/chpassrequest', function (req, res) {
+    var username = req.body.email;
+    var user = new User(null,null,null,null,null);
+    for (var c in CHPASS)
+      if(CHPASS[c].username == username) {
+        CHPASS.splice(c, 1);
+        break;
+      }
+    var chpass = new ChPass(username);
+    var response = {};
+    var auser = null;
+    for (var u in users)
+      if(users[u].username == username) {
+        auser = users[u];
+        break;
+      }
+    if(!auser) {
+        response.err = 703;
+        response.errmsg = "no such user exists";
+        response.body = "no susch user exists";
+        response.cstat = 703;
+    } else {
+        CHPASS.push(chpass);
+        var body = "Hi "+auser.name+",\n";
+        body += "\nClick the link below if you initiated this activity to confirm"+
+        " the password change.\n\nThank you,\nPrimeBox";
+
+        response.body = {username:username};
+        response.cstat = 200;
+        chpass.email = new Email(username, null, null, 'no-reply@primebox.com',
+        "Change of password request", body, URL+"/app/chpassconfirm/"+chpass.hash);
+    }
+    // send email
+
+    res.jsonp(response);
+});
+
+app.get('/app/chpassemail/:id', function(req, res) {
+  var username = req.params.id;
+  var chpass = null;
+  var response = {};
+  for (var u in CHPASS)
+    if(CHPASS[u].username == username) {
+      chpass = CHPASS[u];
+      break;
+    }
+  if(chpass) {
+    response.body = chpass.email;
+    response.cstat = (chpass.confirm == true)? 301 : 302;
+  } else {
+    response.err = 705;
+    response.errmsg = "Invalid change request";
+    response.body = "Invalid change request";
+    response.cstat = 705;
+  }
+
+  res.jsonp(response);
+});
+
+app.get('/app/chpassconfirm/:hash', function (req, res) {
+    var hash = req.params.hash;
+    var chapss = null;
+    var response = {};
+    for (var u in CHPASS)
+      if(CHPASS[u].hash == hash) {
+        chpass = CHPASS[u];
+        break;
+      }
+    if(!chpass) {
+      response.err = 705;
+      response.errmsg = "Invalid change request";
+      response.body = "Invalid change request";
+      response.cstat = 705;
+    } else {
+      chpass.confirm = true;
+      response.body = "Thank you, for confirming";
+      response.cstat = 301;
+    }
+
+    res.jsonp(response);
+});
+
+app.get('/app/chpass/:id', function (req, res) {
+    var username = req.params.id;
+    var chpass = null;
+    var response = {};
+    for (var u in CHPASS)
+      if(CHPASS[u].username == username) {
+        chpass = CHPASS[u];
+        break;
+      }
+    if(!chpass) {
+      response.err = 705;
+      response.errmsg = "Invalid change request";
+      response.body = "Invalid change request";
+      response.cstat = 705;
+    } else {
+      if(chpass.confirm) {
+        response.body = "Confirmed";
+        response.cstat = 301;
+      } else {
+        response.body = "Pending";
+        response.cstat = 302;
+      }
+    }
+
+    res.jsonp(response);
+});
 
 app.listen(app.port, app.ip, function () {
   console.log('server started at '+app.ip+':'+app.port);
