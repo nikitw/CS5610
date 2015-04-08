4c3ccd6d30789f49bfc515c0eebd6cfa6e8b1db6
Nikit Waghela <waghelanikit@gmail.com>
2015-04-08 19:29:11 -0400

    exp 10

---
+++ experiments/week10_exps/js/exp4-controller.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module('TripPlannerApp.controllers', []);
+
+
+app.controller('photosCtrl', function($scope, $location, photosAPI) {
+    $scope.files = photosAPI.getUploads(function(files) {
+        $scope.files = files;
+    });
+
+    $scope.onComplete = function(content) {
+        console.log(JSON.parse(content));
+    }
+});
