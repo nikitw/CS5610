d9e032e9c3a5bcdd3938e783fe0f5670d4855d2a
Nikit Waghela <waghelanikit@gmail.com>
2015-04-19 21:30:18 -0400

    tripPlanner Landing page
    TripPlanner Landing page

---
+++ tripPlanner/js/app.js
@@ -0,0 +1,25 @@
+﻿var app = angular.module('TripPlannerApp', [
+  'ngRoute',
+  'TripPlannerApp.controllers',
+  'TripPlannerApp.services'
+]);
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/home.html',
+        controller: 'ctrl'
+      }).
+      when('/features', {
+        templateUrl: 'partials/features.html',
+        controller: 'ctrl'
+      }).
+      when('/contact', {
+        templateUrl: 'partials/contact.html',
+        controller: 'ctrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ tripPlanner/js/controller.js
@@ -0,0 +1,27 @@
+﻿var app = angular.module('TripPlannerApp.controllers', []);
+
+var $navScope = null;
+app.controller('nav', function($scope) {
+    $navScope = $scope;
+    $navScope.page = null;
+});
+
+app.controller('ctrl', function($scope, $location, $routeParams) {
+    $scope.message = null;
+    $scope.email = null;
+
+    $scope.leaveAMessage = function() {
+        if ($scope.message == '' || $scope.message == null || $scope.email == null || $scope.email == '') {
+            $scope.info = {message: "* marked fields are mandatory"};
+            return;
+        }
+        $scope.info = {message: "your message has been sent successfully, we will get back to you as soon as possible"};
+        $scope.message = null;
+        $scope.email = null;
+
+    };
+
+    $scope.nav = function(page) {
+        $navScope.page = page;
+    }
+});
